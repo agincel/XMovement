da8d4ebd9e09f78a6549120ebc87c7d696e44348
Xenthio <thegooster88@gmail.com>
2024-12-17 16:36:13 +1100

    Use new animation stuff

--- Libraries/XMovement/Code/Example/Complex/PlayerWalkControllerComplex.Body.cs
+++ Libraries/XMovement/Code/Example/Complex/PlayerWalkControllerComplex.Body.cs
@@ -24,21 +24,89 @@ public partial class PlayerWalkControllerComplex : Component
 			AnimationHelper.Target = BodyModelRenderer;
 		}
 	}
-	public virtual void Animate()
-	{
-		var rotateDifference = 0f;
+	[Property, Group( "Animator" )] public float RotationAngleLimit { get; set; } = 45.0f;
+	[Property, Group( "Animator" )] public float RotationSpeed { get; set; } = 1.0f;
+	[Property, Group( "Animator" )] public bool RotationFaceLadders { get; set; } = true;
+	float _animRotationSpeed;
+	TimeSince timeSinceRotationSpeedUpdate;
 
+	public virtual void RotateBody()
+	{
+		if ( IsTouchingLadder && RotationFaceLadders )
+		{
+			Body.WorldRotation = Rotation.Lerp( Body.WorldRotation, Rotation.LookAt( LadderNormal * -1 ), Time.Delta * 5.0f );
+			return;
+		}
 		var targetAngle = new Angles( 0, EyeAngles.yaw, 0 ).ToRotation();
 
-		var turnSpeed = 0.02f;
-		BodyModelRenderer.WorldRotation = Rotation.Slerp( BodyModelRenderer.WorldRotation, targetAngle, Controller.WishVelocity.Length * Time.Delta * turnSpeed );
-		BodyModelRenderer.WorldRotation = BodyModelRenderer.WorldRotation.Clamp( targetAngle, 45.0f, out var shuffle ); // lock facing to within 45 degrees of look direction
-		rotateDifference = shuffle;
+		var velocity = Controller.WishVelocity.WithZ( 0 );
 
+		float rotateDifference = BodyModelRenderer.WorldRotation.Distance( targetAngle );
+
+		// We're over the limit - snap it 
+		if ( rotateDifference > RotationAngleLimit )
+		{
+			var delta = 0.999f - (RotationAngleLimit / rotateDifference);
+			var newRotation = Rotation.Lerp( BodyModelRenderer.WorldRotation, targetAngle, delta );
+
+			var a = newRotation.Angles();
+			var b = BodyModelRenderer.WorldRotation.Angles();
+
+			var yaw = MathX.DeltaDegrees( a.yaw, b.yaw );
+
+			_animRotationSpeed += yaw;
+			_animRotationSpeed = _animRotationSpeed.Clamp( -90, 90 );
+
+			BodyModelRenderer.WorldRotation = newRotation;
+		}
+
+		if ( velocity.Length > 10 )
+		{
+			var newRotation = Rotation.Slerp( BodyModelRenderer.WorldRotation, targetAngle, Time.Delta * 2.0f * RotationSpeed * velocity.Length.Remap( 0, 100 ) );
+
+			var a = newRotation.Angles();
+			var b = BodyModelRenderer.WorldRotation.Angles();
+
+			var yaw = MathX.DeltaDegrees( a.yaw, b.yaw );
+
+			_animRotationSpeed += yaw;
+			_animRotationSpeed = _animRotationSpeed.Clamp( -90, 90 );
+
+			BodyModelRenderer.WorldRotation = newRotation;
+		}
+	}
+	public virtual void Animate()
+	{
 		AnimationHelper.WithWishVelocity( Controller.WishVelocity );
 		AnimationHelper.WithVelocity( Controller.Velocity );
-		AnimationHelper.IsGrounded = Controller.IsOnGround;
+
+		// skid, this isn't in AnimationHelper yet it seems? ok nvm it's absolutely FUCKED
+		/*{
+			var dir = Controller.Velocity.SubtractDirection( Controller.WishVelocity.Normal );
+			if ( dir.IsNearlyZero( 1.0f ) ) dir = 0;
+
+			var forward = BodyModelRenderer.WorldRotation.Forward.Dot( dir );
+			var sideward = BodyModelRenderer.WorldRotation.Right.Dot( dir );
+
+			BodyModelRenderer.Set( "skid_x", forward );
+			BodyModelRenderer.Set( "skid_y", sideward );
+
+			var skidAmount = (Controller.Velocity.Length - Controller.WishVelocity.Length).Clamp( 0, 10 ).Remap( 0, 10, 0, 0.5f );
+			BodyModelRenderer.Set( "skid", skidAmount );
+		}*/
+
 		AnimationHelper.WithLook( EyeAngles.Forward * 100, 1, 1, 1.0f );
 		AnimationHelper.DuckLevel = IsCrouching ? 100 : 0;
+		AnimationHelper.IsGrounded = Controller.IsOnGround || IsTouchingLadder;
+		AnimationHelper.IsClimbing = IsTouchingLadder;
+		AnimationHelper.IsSwimming = IsSwimming;
+
+		if ( timeSinceRotationSpeedUpdate > 0.1f )
+		{
+			timeSinceRotationSpeedUpdate = 0;
+			AnimationHelper.MoveRotationSpeed = _animRotationSpeed * 5;
+			_animRotationSpeed = 0;
+		}
+		RotateBody();
 	}
 }
